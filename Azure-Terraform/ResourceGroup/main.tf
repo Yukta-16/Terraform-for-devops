572178d8099e76c64726056d425d6595d77c8ea7
Yukta-16 <71117539+Yukta-16@users.noreply.github.com>
2026-01-01 07:05:22 +0530

    Azure rg

---
+++ Azure-Terraform/ResourceGroup/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-learning"
+  location = "Central India"
+
+  tags = {
+    environment = "learning"
+  }
+}
